b973196d1ace7999db783e8eaa6b496848352708
Scott Burch <scott@bulldoginfo.com>
2015-12-28 19:14:07 -0800

    updated to jasmine-2.4.1

--- src/browser-jasmine-testing.js
+++ src/browser-jasmine-testing.js
@@ -1,12 +1,12 @@
 bindPolyfill();
 
-require('./lib/jasmine-2.3.4/jasmine.css');
+require('./lib/jasmine-2.4.1/jasmine.css');
 require('es5-shim');
 var scripts = [
     require('source-map-support/browser-source-map-support.js'),
-    require('./lib/jasmine-2.3.4/jasmine.js'),
-    require('./lib/jasmine-2.3.4/jasmine-html.js'),
-    require('./lib/jasmine-2.3.4/boot.js'),
+    require('./lib/jasmine-2.4.1/jasmine.js'),
+    require('./lib/jasmine-2.4.1/jasmine-html.js'),
+    require('./lib/jasmine-2.4.1/boot.js'),
 ].join(';');
 
 
