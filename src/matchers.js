a173e8a7360bb682b234aef809393b02cd71e8cb
Scott Burch <scott@bulldoginfo.com>
2015-12-30 20:18:55 -0800

    added new helpers

--- src/matchers.js
+++ src/matchers.js
@@ -1,12 +1,16 @@
+var _ = require('lodash');
+
 beforeEach(() => {
     jasmine.addMatchers({
-        toHaveLength: makeMatcherFactory(actual => actual.length)
+        toHaveLength: makeMatcherFactory(actual => actual.length),
+        toHaveClass: makeMatcherFactory((actual, expected) => actual.hasClass(expected) ? expected : actual.attr('class')),
+        toContainInBody: makeMatcherFactory((actual, expected) => _.contains(actual.html(), expected) ? expected : actual.html())
     });
 });
 
 
 function makeMatcherFactory(fn) {
     return (util, customEqualityTesters) => ({
-        compare: (actual, expected) => ({pass: util.equals(fn(actual), expected, customEqualityTesters)})
+        compare: (actual, expected) => ({pass: util.equals(fn(actual, expected), expected, customEqualityTesters)})
     });
 }
