93d8f33db511916ca93a2779efcf837d2ea1d1ed
Scott Burch <scott@bulldoginfo.com>
2015-12-30 23:38:11 -0800

    fixed problem with matchers

--- src/matchers.js
+++ src/matchers.js
@@ -2,15 +2,19 @@ var _ = require('lodash');
 
 beforeEach(() => {
     jasmine.addMatchers({
-        toHaveLength: makeMatcherFactory(actual => actual.length),
-        toHaveClass: makeMatcherFactory((actual, expected) => actual.hasClass(expected) ? expected : actual.attr('class')),
-        toContainInBody: makeMatcherFactory((actual, expected) => _.contains(actual.html(), expected) ? expected : actual.html())
+        toHaveLength: makeMatcherFactory('to have length', actual => actual.length),
+        toHaveClass: makeMatcherFactory('to have class',  (actual, expected) => actual.hasClass(expected) ? expected : ''),
+        toContainInBody: makeMatcherFactory('to contain in body', (actual, expected) => _.contains(actual.html(), expected) ? expected : '')
     });
 });
 
 
-function makeMatcherFactory(fn) {
+function makeMatcherFactory(text, fn) {
     return (util, customEqualityTesters) => ({
-        compare: (actual, expected) => ({pass: util.equals(fn(actual, expected), expected, customEqualityTesters)})
+        compare: (actual, expected) => {
+            var pass = util.equals(fn(actual, expected), expected, customEqualityTesters);
+            var message = pass ?  `expected ${actual} not ${text} ${expected}` : `expected ${actual} ${text} ${expected}`
+            return {pass: pass, message: message};
+        }
     });
 }
