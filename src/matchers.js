9d7200f752b7fcb2b0b1788869d6e4e6176c92c3
Scott Burch <scott@bulldoginfo.com>
2015-12-31 07:43:55 -0800

    added new helpers

--- src/matchers.js
+++ src/matchers.js
@@ -4,7 +4,12 @@ beforeEach(() => {
     jasmine.addMatchers({
         toHaveLength: makeMatcherFactory('to have length', actual => actual.length),
         toHaveClass: makeMatcherFactory('to have class',  (actual, expected) => actual.hasClass(expected) ? expected : ''),
-        toContainInBody: makeMatcherFactory('to contain in body', (actual, expected) => _.contains(actual.html(), expected) ? expected : '')
+        toContainInBody: makeMatcherFactory('to contain in body', (actual, expected) => _.contains(actual.html(), expected) ? expected : ''),
+        toHaveFieldWithLabel: makeMatcherFactory('to have field with label', (actual, expected) => {
+            var {name, label} = expected;
+            return actual.find('.input-form_group').filter((idx, el) => $j(el).find(`span:contains(${label})`).length && $j(el).find(`[name="${name}"]`).length).length ? expected : []
+        }),
+        toContainDomEl: makeMatcherFactory('to contain DOM Element', (actual, expected) => actual.find(expected).length ? expected : 'not found')
     });
 });
 
@@ -13,7 +18,11 @@ function makeMatcherFactory(text, fn) {
     return (util, customEqualityTesters) => ({
         compare: (actual, expected) => {
             var pass = util.equals(fn(actual, expected), expected, customEqualityTesters);
-            var message = pass ?  `expected ${actual} not ${text} ${expected}` : `expected ${actual} ${text} ${expected}`
+            try {var expectedString = JSON.stringify(expected)}
+            catch(e) {var expectedString = expected}
+            try {var actualString = JSON.stringify(actual)}
+            catch(e) {var actualString = actual}
+            var message = pass ?  `expected ${actualString} not ${text} ${expectedString}` : `expected ${actualString} ${text} ${expectedString}`
             return {pass: pass, message: message};
         }
     });
