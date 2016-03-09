8338a070cb7f21f12a6dd369829182110b356ac4
Scott Burch <scott@bulldoginfo.com>
2016-03-09 13:34:30 -0800

    added spyOnPrivate() function

--- src/helpers.js
+++ src/helpers.js
@@ -1,8 +1,30 @@
 module.exports = {};
+
 global.waitUntilTrue = fn => {
     return new Promise(function looper(resolve){
         fn() ? resolve() : setTimeout(looper.bind(null, resolve), 10);
     });
 };
+
+global.spyOnPrivate = (function() {
+    var removeSpyFunctions = [];
+
+    afterEach(() => {
+        removeSpyFunctions.forEach(fn => fn());
+        removeSpyFunctions = [];
+    });
+
+
+    return function(obj, prop) {
+        var mySpy = jasmine.createSpy();
+        var oldFn = obj.__get__(prop);
+        obj.__set__(prop, mySpy);
+
+        removeSpyFunctions.push(() => obj.__set__(prop, oldFn));
+
+        return mySpy;
+    }
+
+}());
 
 
